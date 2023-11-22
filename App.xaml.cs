6c6f1f90d88375c0b298e41f71f4681415bdf5af
will2271 <williamskynose9@hotmail.com>
2023-11-22 14:48:55 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace YoutubePlaylist;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+//using static Android.Provider.MediaStore.Audio;
+
+namespace YoutubePlaylist;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+
+    async Task<string> ProcessRepositoriesAsync(HttpClient client)
+    {        
+        var json = await client.GetStringAsync(
+             "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=3&playlistId=PLruRi1CGkSSVlLlQCs9CNusYumuTWIL8z&fields=pageInfo%2C%20items%2Fsnippet(title%2C%20resourceId%2FvideoId)&key=AIzaSyAVpU_4x8B9dKtZZcEhWpli0YOfTACDzFM");
+
+        playList playList = JsonConvert.DeserializeObject<playList>(json);
+        return json;
+    }
+
+    private async void OnCounterClicked(object sender, EventArgs e)
+    {
+        HttpClient client = new HttpClient();
+        CounterBtn.Text = await ProcessRepositoriesAsync(client);
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
+}
+
+public class playList
+{
+    public List<item> items { get; set; }
+}
+
+[Serializable]
+public class item
+{
+    public snippet snippet { get; set; }
+
+}
+
+[Serializable]
+public class snippet
+{
+    public string title { get; set; }
+    public resourceId resourceId { get; set; }
+}
+
+[Serializable]
+public class resourceId
+{
+    public string kind { get; set; }
+    public string videoId { get; set; }
+}
+
